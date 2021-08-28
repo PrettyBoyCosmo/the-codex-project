859a11e4b8a849ecbd202a5dcb949ea207a1bfdd
Blue Cosmo <cosmodiumcs@gmail.com>
2021-08-27 20:08:11 -0500

    one liners modified

--- ciphers/caesar-cipher.py
+++ ciphers/caesar-cipher.py
@@ -3,46 +3,17 @@
 # caesar cipher package for the the codex project
 # created by : C0SM0
 
-# NOTE: if this shit doesn't work, use argparse
-
+# TODO: check if args are empty [try: code, except getopt.GetoptError]
+# TODO: bruteforcing [range, 26]
 # TODO: add colors
-
-# argument parsing
-"""
-print(sys.argv[0])
-print(sys.argv[1])
-
-opts, args = getopt.getopt(sys.argv[1:], "f:m:", ['filename', 'message'])
-
-for opt, arg in opts:
-    if opt == '-f':
-        filename = arg
-    if opt == '-m':
-        message = arg
-"""
-
-# arguments
-# NOTE: incorportate range into -b or -r
-"""
-
-file.py CIPHER[-e,-d,-b] KEY[-k] INPUT[-t,-t] OUTPUT[-o]
-
--e = encrypt
--d = decrypt
--b = bruteforce 
-
--k = key
-
--t = input file [.txt]
--t = input text
--o = output file
-"""
+# TODO: cleanup code
 
 # imports
 import sys
 import getopt
 import getpass
 
+# banner for cli
 banner = """_________                                              _________ .__       .__                  
 \_   ___ \_____    ____   ___________ _______         \_   ___ \|__|_____ |  |__   ___________ 
 /    \  \/\__  \ _/ __ \ /  ___/\__  \\_  __ \  ______ /    \  \/|  \____ \|  |  \_/ __ \_  __ \\
@@ -50,13 +21,24 @@ banner = """_________                                              _________ .__
  \______  (____  /\___  >____  >(____  /__|            \______  /__|   __/|___|  /\___  >__|   
         \/     \/     \/     \/      \/                       \/   |__|        \/     \/     """
 
-# argument variables
-# key = 0
-# input_file = ''
-# input_text = ''
-# output_file = ''
-# output_text = ''
+# help menu for displaying argument options
+help_menu = """
+        Caesar-Cipher oneliners
 
+        First Argument: Ciphering Process
+        -e = encrypt
+        -d = decrypt
+        -b = bruteforce 
+
+        Additional Arguments:
+        -k <integer key> = key
+        -t <plaintext>   = input file [.txt]
+        -i <input file>  = input text
+        -o <output file> = output file [output will be printed to screen by default]
+
+        Example:
+        file.py -e -k 5 -t hello 
+        """
 
 # symbols that can't be processed through the cipher
 symbols = ['\n', '\t', ' ', '.', '?', '!', ',', '/', '\\', '<', '>', '|',
@@ -65,13 +47,13 @@ symbols = ['\n', '\t', ' ', '.', '?', '!', ',', '/', '\\', '<', '>', '|',
            '4', '5', '6', '7', '8', '9']
 
 # generate path
-path = f"{getpass.getuser()}@codex $ "
+path = f"{getpass.getuser()}@caesar-cipher $ "
 
-# encrypts text
+# NOTE: determine whether extra characters are going through
+# encrypts content
 def encrypt_caesar(plain_content, encryption_key, print_cnt):
     # output variable
     output = ''
-    output_text = ''
  
     # encryption process
     for character in plain_content:
@@ -81,44 +63,57 @@ def encrypt_caesar(plain_content, encryption_key, print_cnt):
             output += chr((ord(character) + int(encryption_key) - 65) % 26 + 65)
         else:
             output += chr((ord(character) + int(encryption_key) - 97) % 26 + 97)
-    
-    # NOTE: match lengths of ciphertext to see if extra characters exist
-    # remove extra character
-    # text_length = len(output)
-    # for num in range(0, text_length):
-    #     if num != text_length -1:
-    #         output_text += output[num]
 
-    # output content
+    # output content to cli
     if print_cnt == True:
-        print(output)
+        print(f'Encrypted Content:\n{output}\n')
+
+    # output content to file
+    else:
+        with open(print_cnt, 'w') as f:
+            f.write(output)
+
+# decrypts content
+def decrypt_caesar(plain_content, encryption_key, print_cnt):
+    # output variable
+    output = ''
+ 
+    # encryption process
+    for character in plain_content:
+        if character in symbols:
+            output += character
+        elif character.isupper():
+            output += chr((ord(character) - int(encryption_key) - 65) % 26 + 65)
+        else:
+            output += chr((ord(character) - int(encryption_key) - 97) % 26 + 97)
+
+    # outputs content to cli
+    if print_cnt == True:
+        print(f'Decrypted Content:\n{output}\n')
+
+    # outputs content to file
     else:
         with open(print_cnt, 'w') as f:
             f.write(output)
 
 # parse all arguments
-# TODO: fix me, i won't add all of the args
 def parser():
     opts, args = getopt.getopt(sys.argv[2:], 'k:f:t:o:', ['key', 'inputFile', 'inputText','outputFile'])
     arg_dict = {}
 
-    # TODO: fix this shit
-    # if ('iF' and '-t') in opts:
-    #     pass  # end this shit
-
+    # loop through arguments, assign them to dict [arg_dict]
     for opt, arg in opts:
         if opt == '-k':
             arg_dict['-k'] = int(arg)
         # input options
-        if opt == '-f':
-            arg_dict['-f'] = arg
+        if opt == '-i':
+            arg_dict['-i'] = arg
         if opt == '-t':
             arg_dict['-t'] = arg
         # output options
         if opt == '-o':
             arg_dict['-o'] = arg
 
-    print(arg_dict)
     return arg_dict
 
 # command line interface
@@ -127,7 +122,8 @@ def cli(argument_check):
     print(f'{banner}\n\n')
 
     # check for arguments
-    # TODO: check if args are empty
+
+    # one liners
     if argument_check == True:
         arguments = parser()
 
@@ -136,8 +132,8 @@ def cli(argument_check):
         print_content = True
         
         # checks users output type
-        if ('-f' in arguments):
-            inputted_content = open(arguments.get('-f'), 'r').read()
+        if ('-i' in arguments):
+            inputted_content = open(arguments.get('-i'), 'r').read()
 
         # checks if output was specified
         if ('-o' in arguments):
@@ -145,25 +141,26 @@ def cli(argument_check):
 
         # check ciphering process
         ciphering_process = sys.argv[1]
-        if ciphering_process == '-e':
-            encrypt_caesar(inputted_content, key, print_content)
 
-        if ciphering_process == '-d':
-            pass  # decrypt caesar
+        try:
+            # encrypts caesar
+            if ciphering_process == '-e':
+                encrypt_caesar(inputted_content, key, print_content)
 
-        if ciphering_process == '-b':
-            pass  # bruteforce
+            # decrypts caesar
+            if ciphering_process == '-d':
+                decrypt_caesar(inputted_content, key, print_content)
 
+            # bruteforce caesar
+            if ciphering_process == '-b':
+                pass  # bruteforce
+        
+        except TypeError:
+            print(f'[!!] No Key or Input was specified\n{help_menu}')
+
+    # help menu 
     else:
-        # display options
-        print('Options:\n\teF - Encrypt File\n\teT - Encrypt Text\n\tdF - Decrypt File\n\tdT - Decrypt Text\n\tbF Bruteforce File\n\tbT - Bruteforce Text\n')
-        option = input(f'{path}[~] Option :')
-
-        # option, encrypt file
-        if option == 'eF':
-            plainfile_input = input(f'{path}[~] Enter File Name : ')
-            shift_key = input(f'{path}[~] Enter Encryption Key : ')
-            encrypt_caesar(plainfile_input, int(shift_key))
+        print(help_menu)
 
 # main code
 def caesar_main():
