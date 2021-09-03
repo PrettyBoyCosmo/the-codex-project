f0789a2cc7ed8d12a61fa41819ad2c714f93452d
Blue Cosmo <cosmodiumcs@gmail.com>
2021-09-03 17:16:30 -0500

    working caesar cipher

--- ciphers/caesar-cipher.py
+++ ciphers/caesar-cipher.py
@@ -3,10 +3,9 @@
 # caesar cipher package for the the codex project
 # created by : C0SM0
 
-# TODO: check if args are empty [try: code, except getopt.GetoptError]
-# TODO: bruteforcing [range, 26]
 # TODO: add colors
 # TODO: cleanup code
+# TODO: fix menu
 
 # imports
 import sys
@@ -28,16 +27,17 @@ help_menu = """
         First Argument: Ciphering Process
         -e = encrypt
         -d = decrypt
-        -b = bruteforce 
+        -b = bruteforce [bruteforces using all possible keys by default]
 
         Additional Arguments:
-        -k <integer key> = key
+        -k <integer key> = key [not required for bruteforcing '-b']
+        -r <start,end>   = choose a range of keys to start and end the bruteforce
         -t <plaintext>   = input file [.txt]
         -i <input file>  = input text
         -o <output file> = output file [output will be printed to screen by default]
 
         Example:
-        file.py -e -k 5 -t hello 
+        caesar-cipher.py -e -k 5 -t hello 
         """
 
 # symbols that can't be processed through the cipher
@@ -49,7 +49,6 @@ symbols = ['\n', '\t', ' ', '.', '?', '!', ',', '/', '\\', '<', '>', '|',
 # generate path
 path = f"{getpass.getuser()}@caesar-cipher $ "
 
-# NOTE: determine whether extra characters are going through
 # encrypts content
 def encrypt_caesar(plain_content, encryption_key, print_cnt):
     # output variable
@@ -72,6 +71,8 @@ def encrypt_caesar(plain_content, encryption_key, print_cnt):
     else:
         with open(print_cnt, 'w') as f:
             f.write(output)
+
+    # NOTE: determine whether extra characters are going through
 
 # decrypts content
 def decrypt_caesar(plain_content, encryption_key, print_cnt):
@@ -96,15 +97,47 @@ def decrypt_caesar(plain_content, encryption_key, print_cnt):
         with open(print_cnt, 'w') as f:
             f.write(output)
 
+# bruteforces content
+def bruteforce_caesar(plain_content, print_cnt, start_range=0, end_range=27):
+    # output variable
+    output = ''
+
+    shift_key = start_range
+    for shift in range(start_range, end_range):
+        output += f'Shift Key: {shift_key}\n'
+        shift_key += 1
+
+        for character in plain_content:
+            if character in symbols:
+                output += character
+            elif character.isupper():
+                output += chr((ord(character) - shift - 65) % 26 + 65)
+            else:
+                output += chr((ord(character) - shift - 97) % 26 + 97)
+
+        output += '\n\n'
+
+    # outputs content to cli
+    if print_cnt == True:
+        print(f'Bruteforced Content:\n{output}\n')
+
+    # outputs content to file
+    else:
+        with open(print_cnt, 'w') as f:
+            f.write(output)
+
 # parse all arguments
 def parser():
-    opts, args = getopt.getopt(sys.argv[2:], 'k:f:t:o:', ['key', 'inputFile', 'inputText','outputFile'])
+    opts, args = getopt.getopt(sys.argv[2:], 'k:f:t:o:r:', ['key', 'inputFile', 'inputText', 'outputFile', 'range'])
     arg_dict = {}
 
     # loop through arguments, assign them to dict [arg_dict]
     for opt, arg in opts:
+        # processing options
         if opt == '-k':
             arg_dict['-k'] = int(arg)
+        if opt == '-r':
+            arg_dict['-r'] = arg.split(',')
         # input options
         if opt == '-i':
             arg_dict['-i'] = arg
@@ -121,42 +154,60 @@ def cli(argument_check):
     # display banner
     print(f'{banner}\n\n')
 
-    # check for arguments
-
     # one liners
     if argument_check == True:
-        arguments = parser()
 
-        key = arguments.get('-k')
-        inputted_content = arguments.get('-t')
-        print_content = True
-        
-        # checks users output type
-        if ('-i' in arguments):
-            inputted_content = open(arguments.get('-i'), 'r').read()
-
-        # checks if output was specified
-        if ('-o' in arguments):
-            print_content = arguments.get('-o')
-
-        # check ciphering process
-        ciphering_process = sys.argv[1]
-
+        # tries to get all arguments
         try:
-            # encrypts caesar
-            if ciphering_process == '-e':
-                encrypt_caesar(inputted_content, key, print_content)
+            arguments = parser()
 
-            # decrypts caesar
-            if ciphering_process == '-d':
-                decrypt_caesar(inputted_content, key, print_content)
+        # catches arguments with no value
+        except getopt.GetoptError:
+            print(f'[!!] No value was given to your argument\n{help_menu}')
 
-            # bruteforce caesar
-            if ciphering_process == '-b':
-                pass  # bruteforce
-        
-        except TypeError:
-            print(f'[!!] No Key or Input was specified\n{help_menu}')
+        # continues with recieved arguments
+        else:    
+            # getting variables for ciphering process
+            key = arguments.get('-k')
+            inputted_content = arguments.get('-t')
+            print_content = True
+            
+            # checks users output type
+            if ('-i' in arguments):
+                inputted_content = open(arguments.get('-i'), 'r').read()
+
+            # checks if output was specified
+            if ('-o' in arguments):
+                print_content = arguments.get('-o')
+
+            # checks if range was specified
+            if '-r' in arguments:   
+                range = arguments.get('-r', False)
+
+            # check ciphering process
+            ciphering_process = sys.argv[1]
+
+            # attempts to run cipher
+            try:
+                # encrypts caesar
+                if ciphering_process == '-e':
+                    encrypt_caesar(inputted_content, key, print_content)
+
+                # decrypts caesar
+                if ciphering_process == '-d':
+                    decrypt_caesar(inputted_content, key, print_content)
+
+                # bruteforce caesar
+                if ciphering_process == '-b':
+                    range = range if '-r' in arguments else False
+                    if range == False:
+                        bruteforce_caesar(inputted_content, print_content)
+                    else:
+                        bruteforce_caesar(inputted_content, print_content, int(range[0]), int(range[1])+1)
+
+            # catches unspecified arguments
+            except TypeError:
+                print(f'[!!] No Key or Argument was specified\n{help_menu}')
 
     # help menu 
     else:
@@ -175,5 +226,6 @@ def caesar_main():
 
     cli(arguments_exist)
 
+# runs main function if file is not being imported
 if __name__ == '__main__':
     caesar_main()
---
+++ main.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+# main file for the the codex project
+# created by : C0SM0
